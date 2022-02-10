e23a046ecf886fafc3b67288837089edb88d193d
antonio <voetballergamerajdo@gmail.com>
2022-02-10 12:57:40 +0100

    klaar

--- 4-Arithmetics/4.2 -Assignment Operators/index.php
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -3,7 +3,15 @@
 <body>
 
 <?php
+$getal_1 = 10;
 
+echo $getal_1 += 10;
+echo"<br>";
+echo $getal_1 *= 5;
+echo"<br>";
+echo $getal_1/=2;
+echo "<br>";
+echo $getal_1 -= 4;
 ?>
 
 </body>
--- 5-Conditional_Statements/5.4- if, elseif, else conditonal/index.php
+++ 5-Conditional_Statements/5.4- if, elseif, else conditonal/index.php
@@ -4,7 +4,17 @@
 
 <?php
 
-?>
+$prod_besteld = 40;
 
+if($prod_besteld < 10) {
+    $prijs = 1.50;
+} elseif($prod_besteld < 20) {
+    $prijs = 1.25;
+} else {
+    $prijs = 1.00;
+}
+
+echo $prijs;
+?>
 </body>
 </html> 
