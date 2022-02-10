72ea36de079df4ef050af8591d451f61289cc4e4
antonio <voetballergamerajdo@gmail.com>
2022-02-10 09:42:07 +0100

    klaar

--- 3-Arrays/3.1 - Array variabele/index.php
+++ 3-Arrays/3.1 - Array variabele/index.php
@@ -3,7 +3,8 @@
 <body>
 
 <?php
-
+$mymovies = array("mad max", "sipderman", "batman");
+echo  $mymovies[0] . ", " . $mymovies[1] . ", " . $mymovies[2] ;
 ?>
 
 </body>
--- 3-Arrays/3.3 - Array waarde toevoegen/index.php
+++ 3-Arrays/3.3 - Array waarde toevoegen/index.php
@@ -3,6 +3,11 @@
 <body>
 
 <?php
+$myheroes = array('joker', 'spiderman', 'batman');
+array_push($myheroes, 'superman');
+echo "<pre>";
+print_r($myheroes);
+echo "</pre>";
 
 ?>
 
